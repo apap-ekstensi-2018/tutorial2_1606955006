384bfbda01d305cbe4b35caea5ec30502929c3a1
ruth.nolita <ruth.nolita>
2018-02-20 21:52:54 +0700

    Commit Tutorial 2

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Optional;
+
+
+@Controller
+public class PageController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "hello";
+	}
+	
+	@RequestMapping("/greeting")
+	public String index(@RequestParam(value = "name", required=false) String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+	
+	@RequestMapping({"/greeting/", "greeting/{name}"})
+	public String greetingPath(@PathVariable Optional<String> name, Model model) {
+		if(name.isPresent()) {
+			model.addAttribute("name", name.get());
+		} else {
+			model.addAttribute("name", "apap");
+		}
+		return "greeting";
+	}
+	
+}
